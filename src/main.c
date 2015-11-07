50d20eb308c2dceb0b54a792f6eb2f94772db47d
Michael Spieler <michael.spieler@epfl.ch>
2015-11-07 22:04:17 +0100

    No blinker thread

--- src/main.c
+++ src/main.c
@@ -4,34 +4,11 @@
 #include "usbcfg.h"
 #include "can_bridge.h"
 
-static THD_WORKING_AREA(waThread1, 128);
-static THD_FUNCTION(Thread1, arg) {
-    (void)arg;
-    chRegSetThreadName("blinker");
-    while (true) {
-        palClearPad(GPIOB, GPIOB_LED_GREEN);
-        chThdSleepMilliseconds(500);
-        palSetPad(GPIOB, GPIOB_LED_GREEN);
-        chThdSleepMilliseconds(500);
-    }
-}
-
 void panic(const char *reason)
 {
     (void) reason;
     while (1);
 }
-
-static const CANConfig can1_config = {
-    .mcr = (1 << 6)  /* Automatic bus-off management enabled. */
-         | (1 << 2), /* Message are prioritized by order of arrival. */
-    /* APB Clock is 36 Mhz
-       36MHz / 2 / (1tq + 10tq + 7tq) = 1MHz => 1Mbit */
-    .btr = (1 << 0)  /* Baudrate prescaler (10 bits) */
-         | (9 << 16)/* Time segment 1 (3 bits) */
-         | (6 << 20) /* Time segment 2 (3 bits) */
-         | (0 << 24) /* Resync jump width (2 bits) */
-};
 
 SerialUSBDriver SDU1, SDU2;
 
@@ -39,8 +16,6 @@ int main(void)
 {
     halInit();
     chSysInit();
-
-    chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
 
     // USB CDC
     sduObjectInit(&SDU1);
@@ -60,6 +35,7 @@ int main(void)
     can_bridge_start((BaseSequentialStream *)&SDU1);
 
     while (1) {
-        chThdSleepMilliseconds(1000);
+        chThdSleepMilliseconds(100);
+        led_toggle(STATUS_LED);
     }
 }
